5f0b31651ad58c6fa1c8b1b843b33f43de905b31
Gaurav Yadav <gk5480325@gmail.com>
2022-08-02 19:27:51 +0530

    extended functionality quotes

--- DataTypes.js
+++ DataTypes.js
@@ -13,3 +13,19 @@ console.log(NaN + 3);
 
 let bigInt = 1234567890123456789012345678901234567890n;
 console.log(bigInt);
+
+//String
+
+let str = 'Hello';
+let str2 = "single quotes are ok too!";
+let str3 = `can embed another ${str}`;
+
+console.log(str);
+console.log(str2);
+console.log(str3);
+
+//embeded functionality quotes"BacktIcks"
+
+let user = "john";
+
+console.log(`Hello${user}`);
