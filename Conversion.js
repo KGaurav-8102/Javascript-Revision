48fd56f93e3a36c787cc0249f232e97710e1ee5f
Gaurav Yadav <gk5480325@gmail.com>
2022-08-02 19:48:46 +0530

    Number Conversion

--- Conversion.js
+++ Conversion.js
@@ -5,3 +5,14 @@ console.log(User);
 
 let Man = String(User);
 console.log(Man);
+
+//Number Conversion
+
+console.log("6" / "2");
+
+let str = "123";
+console.log(typeof str); // string
+
+let num = Number(str); // becomes a number 123
+
+console.log(typeof num); // number
