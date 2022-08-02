44b2cf9ea6a2ea3845b1edcb372de3d49293198c
Gaurav Yadav <gk5480325@gmail.com>
2022-08-02 19:35:41 +0530

    Typrof operator

--- DataTypes.js
+++ DataTypes.js
@@ -40,3 +40,23 @@ console.log(isGreater);
 
 let age;
 console.log(age);
+
+//typeof
+
+console.log(typeof undefined); // "undefined"
+
+console.log(typeof 0); // "number"
+
+console.log(typeof 10n); // "bigint"
+
+console.log(typeof true);// "boolean"
+
+console.log(typeof "foo"); // "string"
+
+console.log(typeof Symbol("id")); // "symbol"
+
+console.log(typeof Math); // "object"  (1)
+
+console.log(typeof null); // "object"  (2)
+
+console.log(typeof alert);
