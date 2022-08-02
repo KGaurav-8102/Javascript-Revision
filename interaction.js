5a7fe4a28f934dcb778fedc2b5bf742411b330e7
Gaurav Yadav <gk5480325@gmail.com>
2022-08-02 19:42:05 +0530

    Confirm

--- interaction.js
+++ interaction.js
@@ -9,3 +9,9 @@ alert("Gaurav Yadav");
 let age = prompt('How old are you?', 100);
 
 alert(`You are ${age} years old!`); 
+
+//confirm
+
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed
