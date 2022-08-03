d39ccf688e3a553550ae481a56812d115373ac49
Gaurav Yadav <gk5480325@gmail.com>
2022-08-03 19:55:43 +0530

    Object is Completed

--- Object.js
+++ Object.js
@@ -5,4 +5,27 @@ let user = new Object();
 
 //2. Literal Syntax
 
-let users = {};
+let users = {
+    name: "John",
+    age: 20,
+    "like birds" : true,
+};
+
+//Object propertiese accessible using dot notation
+
+users.isAdmin = true;
+
+console.log(users.name);
+console.log(users.age);
+console.log(users);
+
+delete users.age ;
+console.log(users);
+
+console.log(users["like birds"]);
+console.log("name" in users);
+
+for (let key in users) {
+    console.log(key);
+    console.log(users[key]);
+}
