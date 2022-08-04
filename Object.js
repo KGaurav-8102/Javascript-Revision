2f34b4fe31b50370abd87c1e3a647717a40ee3e1
Gaurav Yadav <gk5480325@gmail.com>
2022-08-04 08:47:25 +0530

    Merging and cloning object

--- Object.js
+++ Object.js
@@ -1,7 +1,7 @@
 //there are two ways to create Object
 //1. using Object constructs
 
-let user = new Object();
+let userm = new Object();
 
 //2. Literal Syntax
 
@@ -29,3 +29,48 @@ for (let key in users) {
     console.log(key);
     console.log(users[key]);
 }
+
+//Object Clonning and merging
+
+let user = {
+    name: 'John',
+    age: 30,
+}
+
+let clone = {};
+
+for (let key in user) {
+    clone[key] = user[key]
+}
+
+clone.name = 'Pete';
+console.log(user.name);
+
+//Object.assign
+
+let kg = {
+    name: 'John',
+};
+
+let permissions1 = { canView: true };
+let permissions2 = { canEdit: true };
+
+Object.assign(kg, permissions1, permissions2);
+console.log(kg);
+
+//deep Cloning
+let gk = {
+    name: "John",
+    sizes: {
+      height: 182,
+      width: 50
+    }
+  };
+  
+  let clones = Object.assign({}, gk);
+  
+  console.log( gk.sizes === clones.sizes ); // true, same object
+  
+  // user and clone share sizes
+  gk.sizes.width++;       // change a property from one place
+  console.log(clones.sizes.width);
