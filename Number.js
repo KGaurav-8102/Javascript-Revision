82f06e6ce1ca94022883032640ac64e5f11822fc
Gaurav Yadav <gk5480325@gmail.com>
2022-08-04 10:37:02 +0530

    Rounding in Numbers

--- Number.js
+++ Number.js
@@ -4,4 +4,17 @@ let bil = 1000000;
 let bil1 = 1_00_00_00;
 let bil2 = 1e6;
 
-//
+//toString(base)
+
+let num = 255;
+
+console.log(num.toString(16));
+console.log(num.toString(8));
+console.log(num.toString(2));
+
+//Rounding
+
+let nums = 1.234567;
+
+console.log(Math.round(nums * 100) / 100);
+console.log(nums.toFixed(2));
